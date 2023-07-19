9664f672be4eb3d04b7732dfa10f98f21b8a80cd
Papersy <v.grishaenko@gmail.com>
2023-07-19 14:04:02 +0200

    no message

--- StoryGraph/Assets/Scripts/Infrastructure/ConstantsData.cs
+++ StoryGraph/Assets/Scripts/Infrastructure/ConstantsData.cs
@@ -3,7 +3,7 @@
     public static class ConstantsData
     {
         public static string HUDAddress = "UI/HUDContainer";
-        public static string PlayerAddress = "Player/PlayerNew";
+        public static string PlayerAddress = "Player/PlayerNew2";
         
         public static string IconsAddress = "JsonFiles/Items/"; 
     }
